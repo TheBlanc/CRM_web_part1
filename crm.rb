f594cc0919a0cf336322ecd4e34968bfbed92a46
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-06-07 23:16:26 -0400

    add contact page with contact info from DB

--- crm.rb
+++ crm.rb
@@ -4,3 +4,17 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+get '/contacts' do
+  @contacts = Contact.all
+  erb :contacts
+end
+
+get '/about' do
+  erb :about
+end
+
+
+after do
+  ActiveRecord::Base.connection.close
+end
